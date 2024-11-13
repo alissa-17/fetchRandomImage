71807382f705dfcfeea8950e57c75020415e4520
Alissa <alissa.work17@aol.com>
2024-11-13 14:46:33 -0500

    update randomly select the index of an image

--- js/script.js
+++ js/script.js
@@ -4,17 +4,16 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function() {
-    const res = await fetch("https://picsum.photos/v2/list?limit=100"
-    );
-    const images = await res .json();
-    console.log(images);
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json();
+    //console.log(images);
     selectRandomImage(images);
 };
-getImage();
+//getImage();
 
-const selectRandomImage = function(images){
-    const randomIndex = Math.floor(Math.random()*images.length);
-  //  console.log(randomIndex);
-    const randomImage = randomIndex();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
     console.log(randomImage);
-}
+};
